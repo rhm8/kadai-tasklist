11049108885f6f615a0e7c673b31ed3d4a49c725
rhm8 <rk-hn-mm-82@rakuten.jp>
2019-06-19 12:46:17 +0000

    CRUD messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_044729) do
+ActiveRecord::Schema.define(version: 2019_06_19_115328) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "Status"
+    t.string "status"
   end
 
 end
