848250fd00ebb48ff8bc17f68d32a3bc14f96af4
rhm8 <rk-hn-mm-82@rakuten.jp>
2019-06-27 13:31:03 +0000

    correct

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,8 @@ class TasksController < ApplicationController
     redirect_to root_url
   end
   
+  private
+  
   def task_params
     params.require(:task).permit(:content, :status, :user)
   end
